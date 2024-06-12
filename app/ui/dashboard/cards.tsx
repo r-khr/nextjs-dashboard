810333dc30b64a1424cc1d632bf5af2a419c080b
Roman Khrystynych <roman.khrystynych@gmail.com>
2024-06-12 13:58:22 -0400

    chap 7

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,8 +16,6 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
-
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
