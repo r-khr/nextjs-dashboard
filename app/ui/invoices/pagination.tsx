867e487382256a6de652e9a21d93330be7a43acd
Roman Khrystynych <roman.khrystynych@gmail.com>
2024-06-13 11:56:38 -0400

    chap 12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -7,7 +7,6 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: comment in this code when you get to this point in the course
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
